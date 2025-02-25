272c28e12314159180f3b7459e3986718448b83b
AnyaRomanyk <ann.romanuk.06@gmail.com>
2025-02-25 21:50:31 +0200

    Add files via upload

---
+++ lab_3/lab3.py
@@ -0,0 +1,17 @@
+def cal_value(line):
+    digits = ""
+    for char in line:
+        if char.isdigit():
+            digits += char
+
+    if digits:
+        return int(digits[0] + digits[-1]) 
+    
+    return 0
+
+with open("input_3.txt", "r", encoding="utf-8") as file:
+    total_sum = sum(cal_value(line) for line in file)
+
+print("Сума всіх калібрувальних значень:", total_sum)
+
+#result: 55538
